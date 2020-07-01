3afe6588542897e9d98b4e001ab6d75c361147ad
vgostyahumarki <43582277+vgostyahumarki@users.noreply.github.com>
2020-07-02 01:39:05 +0300

    Update install.sh
    ffmpeg directory version

--- install.sh
+++ install.sh
@@ -91,4 +91,4 @@ wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
 wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz.md5
 md5sum -c ffmpeg-git-amd64-static.tar.xz.md5
 tar xvf ffmpeg-git-amd64-static.tar.xz
-sudo mv ffmpeg-git-20190305-amd64-static/ffmpeg ffmpeg-git-20190305-amd64-static/ffprobe /usr/local/bin/
+sudo mv ffmpeg-git-20200617-amd64-static/ffmpeg ffmpeg-git-20200617-amd64-static/ffprobe /usr/local/bin/
